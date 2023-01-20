96ad8b3569f4e0ac0487c10ac5f059b63307576d
pyaeger <patrickometry@gmail.com>
2023-01-19 19:58:12 -0500

    now actually adding comments

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,11 @@
 print('Hello Big Big World!')
+
+# git clone (download the repo to your pc)
+
+# git status (see what's up)
+
+# git add <filename> (ready to send this file)
+
+# git commit -m <a commit message> (save my work)
+
+# git push (save my work to github)

ba5238a50d57638370fc0305f0a95ec33586c0e0
pyaeger <patrickometry@gmail.com>
2023-01-26 18:41:29 -0500

    added new code and comments

--- FooBar.py
+++ FooBar.py
@@ -1,1 +1,21 @@
 print('It is all Foobar!')
+
+
+number = 10
+print('number =', number)
+
+person = 'John Doe'
+print(person)
+
+
+
+
+# git clone (download the repo to your pc)
+
+# git status (see what's up)
+
+# git add <filename> (ready to send this file)
+
+# git commit -m <a commit message> (save my work)
+
+# git push (save my work to github)
